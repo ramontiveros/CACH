921e41684cc4e0be7ddaa2c0326e5e223336158a
Unknown <rixtrevizo@gmail.com>
2016-11-03 09:40:42 -0700

    auth event?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import {AngularFire} from "angularfire2";
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>'
 })
 export class AppComponent {
-
+  constructor(af: AngularFire) {
+    af.auth.subscribe(auth => {
+      if(auth) {
+        console.log('logged in');
+      } else {
+        console.log('not logged in');
+      }
+    });
+  }
 }
 
