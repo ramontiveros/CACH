90102901365ff5529dfd928293e886649da6ee40
Unknown <rixtrevizo@gmail.com>
2016-11-09 10:35:00 -0700

    ng firebase
    ng firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-
+import {FirebaseService} from 'ng2-firebase/core';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>'
+  template: 'hai'
 })
-export class AppComponent{ }
+export class AppComponent{
+  constructor(private firebase: FirebaseService<any>) {
+  }
+}
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,34 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterializeModule } from 'angular2-materialize';
-
-import { AppRoutingModule } from './app-routing.module';
+import {FirebaseModule} from 'ng2-firebase/core';
 import { AppComponent } from './app.component';
-
-import { AuthModule } from './auth/auth.module';
-
-import { MainModule } from './main/main.module';
-import { AuthService } from './auth.service';
-
-
-import { AngularFireModule, AuthProviders, AuthMethods  } from 'angularfire2';
-import { AuthGuard } from "./auth-guard.service";
-import { LoginGuard } from "./login-guard.service";
-
-
 
 
 export const firebaseConfig = {
     apiKey: "AIzaSyBdgdpyGBHWA4_3Uq9Ig52gpyo9SagSAf0",
     authDomain: "chas-itesm.firebaseapp.com",
-    databaseURL: "https://chas-itesm.firebaseio.com",
+    url: "https://chas-itesm.firebaseio.com",
     storageBucket: "chas-itesm.appspot.com",
     messagingSenderId: "841229532620"
   };
-export const firebaseAuthConfig = {
-  provider: AuthProviders.Password,
-  method: AuthMethods.Password
-  };
+
 
 
 @NgModule({
@@ -41,16 +25,11 @@ export const firebaseAuthConfig = {
     BrowserModule,
     FormsModule,
     HttpModule,
-	  AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig),
-    AppRoutingModule,
-    AuthModule,
-    MainModule,
+    FirebaseModule.forRoot(firebaseConfig),
     MaterializeModule,
   ],
   providers: [
-      AuthService,
-    AuthGuard,
-    LoginGuard
+
     ],
   bootstrap: [AppComponent]
 })
