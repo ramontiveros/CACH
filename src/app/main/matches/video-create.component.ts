a6804532f946e1abb0deb403ea23070f6250f93e
Unknown <rixtrevizo@gmail.com>
2016-11-16 13:28:05 -0700

    UX de subir video completa

--- src/app/main/matches/video-create.component.ts
+++ src/app/main/matches/video-create.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FirebaseService, FirebaseArray } from 'ng2-firebase/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Video } from './video.component';
+import { Router } from '@angular/router';
+
 
 declare var firebase;
 
@@ -12,16 +14,47 @@ declare var firebase;
 })
 export class VideoCreateComponent implements OnInit {
     file: File;
-    constructor(private firebaseService: FirebaseService<Video>) { }
+    barProgress: number;
+    buttonDisabled: boolean;
+    uploading = false;
+    constructor(private firebaseService: FirebaseService<Video>,
+                private router: Router) {}
 
     ngOnInit() { }
 
     onSubmit(video: Video) {
-        let newRef = firebase.database().ref().child('videos').push(video),
+      this.buttonDisabled = true;
+      let component = this;
+
+      let newRef = firebase.database().ref().child('videos').push(video),
             key = newRef.key,
             fileRef = firebase.storage().ref().child('videos/' + key + ".mp4"),
             fileData = this.file;
-        fileRef.put(fileData);
+        let uploadTask = fileRef.put(fileData);
+        component.uploading = true;
+
+      uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+        function(snapshot) {
+          component.barProgress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        }, function(error) { //callback when error
+          switch (error.code) {
+            case 'storage/unauthorized':
+              console.log("unauthorized");
+              break;
+            case 'storage/canceled':
+              console.log("upload canceled");
+              break;
+            case 'storage/unknown':
+              console.log("something bad happened");
+              break;
+          }
+          component.buttonDisabled = false;
+
+        }, function() { //callback when success
+          component.buttonDisabled = false;
+          component.uploading = false;
+          component.router.navigateByUrl('/app/videos/view/' + key);
+        });
     }
 
     readFile($event) : void {
